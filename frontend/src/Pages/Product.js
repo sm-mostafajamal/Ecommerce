14c75c6a7806c2d863df4d7f46c945f7c0210a98
Mostafa <sm.mostafajamal@gmail.com>
2023-03-09 08:06:43 +0600

    deleted

--- frontend/src/Pages/Product.js
+++
@@ -1,6 +0,0 @@
-import { useParams } from "react-router-dom";
-const Product = () => {
-  const id = useParams().id;
-  return <div>{id}</div>;
-};
-export default Product;
--- frontend/src/Services/ecommerceServer.js
+++
@@ -1,4 +0,0 @@
-import axios from "axios";
-const baseURL = "/api/products";
-
-export const getAll = () => axios.get(baseURL).then((res) => res.data);
