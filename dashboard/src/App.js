1a0806d92f4f03b288946341f34ffb8a21ac6a91
sm-mostafajamal <sm.mostafajamal@gmail.com>
2023-05-08 05:26:26 +0600

    fixed bug

--- dashboard/src/App.js
+++ dashboard/src/App.js
@@ -13,41 +13,43 @@ import { DarkModeContext } from "./context/DarkModeContext";
 function App() {
   const { state } = useContext(DarkModeContext);
   const admin = JSON.parse(
-    JSON.parse(localStorage.getItem("persist:root")).user
-  ).currentUser.isAdmin;
+    JSON.parse(localStorage.getItem("persist:root"))?.user || "{}"
+  )?.currentUser?.isAdmin;
   // const admin = null;
   return (
     <div className={state.darkMode ? "app dark" : "app light"}>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Navigate replace to="/login" />} >
-            {admin === null ? (
-
-              <>
-                <Route path="/login" element={<Login />} />
-                <Route index element={<Home />} />
-                <Route path="*" element={<Navigate to="/" replace />} />
-                <Route path="users">
-                  <Route index element={<Lists />} />
-                  <Route path=":userId" element={<Single />} />
-                  <Route
-                    path="new"
-                    element={<New inputs={userInputs} title="Add New User" />}
-                  />
-                </Route>
-                <Route path="products">
-                  <Route index element={<Lists />} />
-                  <Route path=":productId" element={<Single />} />
-                  <Route
-                    path="new"
-                    element={
-                      <New inputs={productInputs} title="Add New Product" />
-                    }
-                  />
-                </Route>
-              </>
-            )}
-          </Route>
+          {admin === null ? (
+            <>
+              <Route path="/" element={<Navigate replace to="/login" />} />
+              <Route path="/login" element={<Login />} />
+            </>
+          ) : (
+            <>
+              <Route index element={<Home />} />
+              <Route path="*" element={<Navigate to="/" replace />} />
+              <Route path="users">
+                <Route index element={<Lists />} />
+                <Route path=":userId" element={<Single />} />
+                <Route
+                  path="new"
+                  element={<New inputs={userInputs} title="Add New User" />}
+                />
+              </Route>
+              <Route path="products">
+                <Route index element={<Lists />} />
+                <Route path=":productId" element={<Single />} />
+                <Route
+                  path="new"
+                  element={
+                    <New inputs={productInputs} title="Add New Product" />
+                  }
+                />
+              </Route>
+            </>
+          )}
+          {/* </Route> */}
         </Routes>
       </BrowserRouter>
     </div>
--- dashboard/src/services/dashboard.js
+++ dashboard/src/services/dashboard.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 const base_Url = "http://localhost:3001/api/";
-const TOKEN = JSON.parse(JSON.parse(localStorage.getItem("persist:root")).user)
-  .currentUser.accessToken;
+const TOKEN = JSON.parse(
+  JSON.parse(localStorage.getItem("persist:root"))?.user || "{}"
+)?.currentUser?.accessToken;
 console.log(TOKEN);
 export const publicRequest = axios.create({
   baseURL: base_Url,
